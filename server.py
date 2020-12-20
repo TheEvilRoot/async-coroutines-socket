079e9e45fe29386f7bb869171d45e4d985b9b9f0
TheEvilRoot <robot-creatix@ya.ru>
2020-12-20 18:55:33 +0300

    less complicated example

--- server.py
+++ server.py
@@ -6,48 +6,21 @@ import datetime
 
 serv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 serv.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-serv.bind(('0.0.0.0', 8888))
+serv.bind(('0.0.0.0', 9999))
 serv.listen()
 
-last_ping = None
+def client_thread(sock):
+        while True:
+                input("awaiting for next command...")
+                print('sending 16 bytes...')
+                sock.sendall(b'\xfa' * 0x10)
+                print('16 bytes sent')
 
-def ping_thread(sock, idx):
-	global last_ping
-	while True:
-		time.sleep(int(random.random() * 10))
-		sock.sendall(b'\xaa'*2048)
-		last_ping = datetime.datetime.now()
-
-def client_thread(sock, idx):
-	global last_ping
-	#handshake
-	for i in range(3):
-		sock.sendall(b'\x01'*2048)
-	thr = threading.Thread(target=ping_thread, args=(sock, idx))
-	thr.start()
-	while True:
-		d = sock.recv(2048)
-		if not d:
-			break
-		if d[0] == 0xaa: 
-			if last_ping is None:
-				sock.sendall(b'\xaa'*2048)
-			else:
-				print('ping time:', datetime.datetime.now() - last_ping)
-				last_ping = None
-		else:
-			sock.sendall(b'\xbb'*2048)
-	print(idx, 'is done')
-			
-		
-
-index = 0
 while True:
 	print('awaiting client...')
 	sock, addr = serv.accept()
 	print('client arrived from', addr)
-	thr = threading.Thread(name='client' + str(index), target=client_thread, args=(sock, index))
+	thr = threading.Thread(name='client', target=client_thread, args=(sock, ))
 	thr.start()
-	index += 1
 	
 
--- src/main/kotlin/Core.kt
+++ src/main/kotlin/Core.kt
@@ -1,71 +1,46 @@
 import kotlinx.coroutines.*
-import kotlinx.coroutines.channels.Channel
 import java.net.InetSocketAddress
 import java.nio.ByteBuffer
 import java.nio.channels.AsynchronousSocketChannel
 
-suspend fun createAsync(readDispatcher: CoroutineDispatcher, writeDispatcher: CoroutineDispatcher, id: Int): Deferred<Unit> {
-    val println = { s: String -> println("${Thread.currentThread().name} :  $id: $s") }
+fun ByteArray.hexlify(): String =
+    joinToString(" ") { String.format("%02x", it) }
 
-    val channel = AsynchronousSocketChannel.open()
+suspend fun run(channel: AsynchronousSocketChannel, dispatcher: CoroutineDispatcher) = withContext(dispatcher) {
     val socket = CoroutineSocket(channel)
-    val addr = InetSocketAddress("localhost", 8888)
-    println("connecting...")
-    socket.connect(addr)
-    println("connected")
 
-    val queue = Channel<Byte>()
+    launch {
+        socket.connect(InetSocketAddress("localhost", 9999))
 
-    GlobalScope.launch(readDispatcher) {
-        val buffer = ByteBuffer.allocate(2048)
-
-        var index = 0
+        val buffer = ByteBuffer.allocate(16)
         while (true) {
-            buffer.position(0)
-
+            println("reading...")
             val count = socket.read(buffer)
-            if (count < 2048) {
-                println("read failure: $count < 2048")
+            println("read finished!")
+            if (count <= 0) {
+                println("socket read failure $count")
                 break
             }
-
-            when (buffer[0]) {
-                0xaa.toByte() -> { println("got ping"); queue.send(0xaa.toByte()) }
-                0xbb.toByte() -> println("got pong")
-                else -> println("got message")
-            }
-
-            if (++index == 4) {
-                byteArrayOf(0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7, 0xaa.toByte(), 0x8)
-                    .forEach { queue.send(it) }
-            }
+            println("socket: " + buffer.array().hexlify())
+            buffer.clear()
         }
     }
 
-    return GlobalScope.async(writeDispatcher) {
+    launch {
         while (true) {
-            val byte = queue.receive()
-            val arr = (0 until 2048).map { byte }.toByteArray()
-
-            if (byte == 0xaa.toByte())
-                println("responding pong")
-
-            val count = socket.write(ByteBuffer.wrap(arr))
-            if (count < 2048) {
-                println("write failure : $count < 2048")
-                break
-            }
+            println("ping?!")
+            delay(500)
+            println("pong!!")
             delay(500)
         }
     }
-
 }
 
+@ObsoleteCoroutinesApi
 fun main() {
-    val read = newFixedThreadPoolContext(1, "read-thread")
-    val write = newFixedThreadPoolContext(1, "write-thread")
+    val thread = newFixedThreadPoolContext(1, "thread")
+    val channel = AsynchronousSocketChannel.open()
     runBlocking {
-        (0 until 10).map { createAsync(read, read, it) }
-            .awaitAll()
+        run(channel, thread)
     }
 }
