db4e9fb6aea0661bc17adcfab4e1b174f4f5e987
TheEvilRoot <robot-creatix@ya.ru>
2020-12-26 16:20:55 +0300

    maven publish

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,12 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
+    `maven-publish`
     kotlin("jvm") version "1.4.10"
 }
 
 group = "me.theevilroot"
-version = "1"
+version = "1.0"
 
 repositories {
     mavenCentral()
@@ -16,6 +17,26 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.2")
 }
 
-tasks.withType<KotlinCompile>() {
+val sourcesJar by tasks.registering(Jar::class) {
+    classifier = "sources"
+    from(sourceSets.main.get().allSource)
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = "me.theevilroot"
+            artifactId = "coroutine-async-socket"
+            version = version
+            from(components["java"])
+        }
+        register("mavenJava", MavenPublication::class) {
+            from(components["java"])
+            artifact(sourcesJar.get())
+        }
+    }
+}
+
+tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
 }
