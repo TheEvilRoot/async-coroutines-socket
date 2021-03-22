d8d74a1894f3215e6eeb23baadc589e9586071d0
theevilroot <robot-creatix@ya.ru>
2021-03-22 16:28:00 +0300

    set compatibility to 1.8

--- build.gradle.kts
+++ build.gradle.kts
@@ -41,3 +41,8 @@ publishing {
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
 }
+
+tasks.withType<JavaCompile> {
+    sourceCompatibility = "1.8"
+    targetCompatibility = "1.8"
+}
